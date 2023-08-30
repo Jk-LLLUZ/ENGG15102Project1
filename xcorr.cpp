882ede2dd63907074b3ce76a2a2831796c4f6b65
Katrice Mendoza <katrice.mendoza@obf.ateneo.edu>
2023-08-30 20:56:32 +0800

    Create xcorr.cpp

---
+++ xcorr.cpp
@@ -0,0 +1,25 @@
+//Katrice Annika Mendoza
+//Ezekiel Thomas Laygo
+//John Kyle Lluz
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void parseFromFile()
+{
+ //parse from file to vector
+}
+
+void normCrossCorr()
+{
+ //calculations go here
+}
+
+int main(int argc, char* argv[])
+{
+ //main
+
+ return 0;
+}
