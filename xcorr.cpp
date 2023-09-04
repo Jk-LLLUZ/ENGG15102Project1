c860ef94604ccfb4811198411b39900ef844b891
Katrice Mendoza <katrice.mendoza@obf.ateneo.edu>
2023-09-05 00:52:20 +0800

    command line input validation

--- xcorr.cpp
+++ xcorr.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+string xDataFileName, yDataFileName, outputFileName;
+
 void parseFromFile()
 {
  //parse from file to vector
@@ -20,6 +22,23 @@ void normCrossCorr()
 int main(int argc, char* argv[])
 {
  //main
+ if (argc == 4)
+ {
+  cout << "received file " << argv[1] << " for use as x values" << endl;
+  cout << "received file " << argv[2] << " for use as y values" << endl;
+  cout << "received file " << argv[3] << " for use as the output file" << endl;
+ } else
+ {
+  cout << "please call the program in the format \n > xcorr [xdata] [ydata] [outputfile] \n where each of the bracketed terms are the respective file names." << endl;
+
+  return 0;
+ }
+
+ xDataFileName = argv[1];
+ yDataFileName = argv[2];
+ outputFileName = argv[3];
+
+ //call the file parser here
 
  return 0;
 }
