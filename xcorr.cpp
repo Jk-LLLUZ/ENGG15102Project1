874a68d56ed4e4721c4fa041cd49ebde8326fb4c
Zeke Laygo <zekelaygo@yahoo.com>
2023-09-05 19:13:47 +0800

    parse
    pase

--- xcorr.cpp
+++ xcorr.cpp
@@ -4,15 +4,97 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
 
 using namespace std;
 
 string xDataFileName, yDataFileName, outputFileName;
 
-void parseFromFile()
-{
- //parse from file to vector
+
+struct Record {
+    int xraw;
+    int yraw;
+};
+
+void parseFromFile() {
+    Record record;
+
+    // Get the names of the x and y value files
+    cout << "What is the name of the x value file?" << endl;
+    cin >> record.xname;
+    record.xname += ".txt"; // Append ".txt" to the filename
+
+    cout << "What is the name of the y value file?" << endl;
+    cin >> record.yname;
+    record.yname += ".txt"; // Append ".txt" to the filename
+
+    vector<Record> records;
+
+    // Parse the x value file
+    ifstream xInputFile(record.xname);
+    if (!xInputFile) {
+        cerr << "Error opening the x value file." << endl;
+        return;
+    }
+
+    string line;
+    while (getline(xInputFile, line)) {
+        try {
+            // Convert the line to an integer and store it in the vector
+            int xValue = stoi(line);
+            Record newRecord;
+            newRecord.xraw = xValue;
+            records.push_back(newRecord);
+        } catch (const invalid_argument& e) {
+            cerr << "Invalid data found in x value file: " << line << endl;
+        }
+    }
+
+    xInputFile.close();
+
+    // Parse the y value file
+    ifstream yInputFile(record.yname);
+    if (!yInputFile) {
+        cerr << "Error opening the y value file." << endl;
+        return;
+    }
+
+    while (getline(yInputFile, line)) {
+        try {
+            // Convert the line to an integer and store it in the vector
+            int yValue = stoi(line);
+            // Check if there is a corresponding x value for this y value
+            if (!records.empty()) {
+                records.back().yraw = yValue;
+            } else {
+                cerr << "No corresponding x value found for y value: " << yValue << endl;
+            }
+        } catch (const invalid_argument& e) {
+            cerr << "Invalid data found in y value file: " << line << endl;
+        }
+    }
+
+    yInputFile.close();
+
+    // Now, 'records' vector contains the parsed data
+    for (const Record& rec : records) {
+        cout << "xraw: " << rec.xraw << ", yraw: " << rec.yraw << endl;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 void normCrossCorr()
 {
