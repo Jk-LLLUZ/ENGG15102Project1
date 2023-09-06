5f062a6a2a3cf6e01b9d210ec108875dd99b2ebd
Zeke Laygo <zekelaygo@yahoo.com>
2023-09-06 23:28:39 +0800

    update

--- xcorr.cpp
+++ xcorr.cpp
@@ -10,35 +10,18 @@
 
 using namespace std;
 
-string xDataFileName, yDataFileName, outputFileName;
-
-
 struct Record {
     int xraw;
     int yraw;
 };
-
-void parseFromFile() {
-    Record record;
-
-    // Get the names of the x and y value files
-    cout << "What is the name of the x value file?" << endl;
-    cin >> xDataFileName;
-    xDataFileName += ".txt"; // Append ".txt" to the filename
-
-    cout << "What is the name of the y value file?" << endl;
-    cin >> yDataFileName;
-    yDataFileName += ".txt"; // Append ".txt" to the filename
-
+vector<Record> parseFromFiles(const string& xDataFileName, const string& yDataFileName) {
     vector<Record> records;
-
     // Parse the x value file
     ifstream xInputFile(xDataFileName);
     if (!xInputFile) {
         cerr << "Error opening the x value file." << endl;
-        return;
+        return records;
     }
-
     string line;
     while (getline(xInputFile, line)) {
         try {
@@ -51,76 +34,60 @@ void parseFromFile() {
             cerr << "Invalid data found in x value file: " << line << endl;
         }
     }
-
     xInputFile.close();
-
     // Parse the y value file
     ifstream yInputFile(yDataFileName);
     if (!yInputFile) {
         cerr << "Error opening the y value file." << endl;
-        return;
+        return records;
     }
-
+    size_t currentIndex = 0; // Keep track of the current index in 'records'
     while (getline(yInputFile, line)) {
         try {
             // Convert the line to an integer and store it in the vector
             int yValue = stoi(line);
-            // Check if there is a corresponding x value for this y value
-            if (!records.empty()) {
-                records.back().yraw = yValue;
+            if (currentIndex < records.size()) {
+                // Assign the y value to the corresponding x value
+                records[currentIndex].yraw = yValue;
+                currentIndex++;
             } else {
-                cerr << "No corresponding x value found for y value: " << yValue << endl;
+                cerr << "More y values found than x values." << endl;
+                break; // Stop parsing if there are more y values than x values
             }
         } catch (const invalid_argument& e) {
             cerr << "Invalid data found in y value file: " << line << endl;
         }
     }
-
     yInputFile.close();
-
+    return records;
+}
+void normCrossCorr()
+{
+    //calculations go here
+}
+int main(int argc, char* argv[]) {
+    cout
+            << "please call the program in the format \n > xcorr [xdata] [ydata] [outputfile] \n where each of the bracketed terms are the respective file names."<< endl;
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] << " [xdata] [ydata] [output file]" << endl;
+        return 1;
+    }
+    string programName = argv[0];
+    string xDataFileName = argv[1];
+    string yDataFileName = argv[2];
+    string outputFileName = argv[3];
+    if (programName != "xcorr") {
+        cerr << "Usage: " << programName << " [xdata] [ydata] [output file]" << endl;
+        return 1;
+    }
+    vector<Record> records = parseFromFiles(xDataFileName, yDataFileName);
     // Now, 'records' vector contains the parsed data
     for (const Record& rec : records) {
         cout << "xraw: " << rec.xraw << ", yraw: " << rec.yraw << endl;
     }
+    // Implement cross-correlation here using 'records'
+    return 0;
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-void normCrossCorr()
-{
- //calculations go here
-}
-
-int main(int argc, char* argv[])
-{
- //main
- if (argc == 4)
- {
-  cout << "received file " << argv[1] << " for use as x values" << endl;
-  cout << "received file " << argv[2] << " for use as y values" << endl;
-  cout << "received file " << argv[3] << " for use as the output file" << endl;
- } else
- {
-  cout << "please call the program in the format \n > xcorr [xdata] [ydata] [outputfile] \n where each of the bracketed terms are the respective file names." << endl;
-
-  return 0;
- }
-
- xDataFileName = argv[1];
- yDataFileName = argv[2];
- outputFileName = argv[3];
-
- //call the file parser here
-
- return 0;
-}
