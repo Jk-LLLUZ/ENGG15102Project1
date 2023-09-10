ced371e710e80d1f921399a76cfbfc560b3b8bce
Zeke Laygo <zekelaygo@yahoo.com>
2023-09-10 23:40:35 +0800

    duration added

--- xcorr.cpp
+++ xcorr.cpp
@@ -42,7 +42,7 @@ class Signal
  public:
   vector<double> signalData;
   int index;
-  int duration;
+  size_t duration;
 
   void parseFromFile(string inputfile)
   {
@@ -89,61 +89,14 @@ class Signal
     }
     
     //parse the rest into signalData
+    
     //set duration according to how big the vector is
+    duration = signalData.size();
+    cout << "Duration is "<< duration << endl;
   }
 };
 
-// struct Record {
-//     int xraw;
-//     int yraw;
-// };
-// vector<Record> parseFromFiles(const string& xDataFileName, const string& yDataFileName) {
-//     vector<Record> records;
-//     // Parse the x value file
-//     ifstream xInputFile(xDataFileName);
-//     if (!xInputFile) {
-//         cerr << "Error opening the x value file." << endl;
-//         return records;
-//     }
-//     string line;
-//     while (getline(xInputFile, line)) {
-//         try {
-//             // Convert the line to an integer and store it in the vector
-//             int xValue = stoi(line);
-//             Record newRecord;
-//             newRecord.xraw = xValue;
-//             records.push_back(newRecord);
-//         } catch (const invalid_argument& e) {
-//             cerr << "Invalid data found in x value file: " << line << endl;
-//         }
-//     }
-//     xInputFile.close();
-//     // Parse the y value file
-//     ifstream yInputFile(yDataFileName);
-//     if (!yInputFile) {
-//         cerr << "Error opening the y value file." << endl;
-//         return records;
-//     }
-//     size_t currentIndex = 0; // Keep track of the current index in 'records'
-//     while (getline(yInputFile, line)) {
-//         try {
-//             // Convert the line to an integer and store it in the vector
-//             int yValue = stoi(line);
-//             if (currentIndex < records.size()) {
-//                 // Assign the y value to the corresponding x value
-//                 records[currentIndex].yraw = yValue;
-//                 currentIndex++;
-//             } else {
-//                 cerr << "More y values found than x values." << endl;
-//                 break; // Stop parsing if there are more y values than x values
-//             }
-//         } catch (const invalid_argument& e) {
-//             cerr << "Invalid data found in y value file: " << line << endl;
-//         }
-//     }
-//     yInputFile.close();
-//     return records;
-// }
+
 void normCrossCorr()
 {
     //calculations go here
