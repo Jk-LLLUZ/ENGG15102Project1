f21c00d1eddbf4fdc70e9a38a5c209c6c2a7e264
Zeke Laygo <zekelaygo@yahoo.com>
2023-09-11 00:10:39 +0800

    y added

--- xcorr.cpp
+++ xcorr.cpp
@@ -40,65 +40,124 @@ bool firstLineInputValidation(string lineToCheck)
 class Signal
 {
  public:
-  vector<double> signalData;
-  int index;
-  size_t duration;
+  vector<double> signalData1;
+  vector<double> signalData2;
+  int index1;
+  int index2;
+  size_t duration1;
+  size_t duration2;
 
-  void parseFromFile(string inputfile)
+  void parseFromFile(string inputfile1, string inputfile2)
   {
     //parse and input validate first line
-    ifstream fileToParse(inputfile);
+    ifstream fileToParse1(inputfile1);
 
-    if(!fileToParse)
+    if(!fileToParse1)
     {
-     cout << "could not open " << inputfile << " to be parsed" << endl;
+     cout << "could not open " << inputfile1 << " to be parsed" << endl;
     } else
     {
      //first line parsing
-     string firstLine;
+     string firstLine1;
 
-     getline(fileToParse, firstLine);
+     getline(fileToParse1, firstLine1);
 
-     cout << "first line: \t" << firstLine << endl;
+     cout << "first line: \t" << firstLine1 << endl;
 
-     bool isThereIndex = firstLineInputValidation(firstLine);
+     bool isThereIndex1 = firstLineInputValidation(firstLine1);
 
-     if (isThereIndex == 1)
+     if (isThereIndex1 == 1)
      {
       cout << "yes theres index" << endl;
 
-      istringstream divideFirstLine(firstLine);
+      istringstream divideFirstLine(firstLine1);
       
-      string strIndex, strFirstValue;
-      divideFirstLine >> strIndex >> strFirstValue;
+      string strIndex1, strFirstValue1;
+      divideFirstLine >> strIndex1 >> strFirstValue1;
 
-      cout << "index: " << strIndex << "\t first: " << strFirstValue << endl;
+      cout << "index: " << strIndex1 << "\t first: " << strFirstValue1 << endl;
 
-      this->index = stoi(strIndex);
-      signalData.push_back(stod(strFirstValue));
+      this->index1 = stoi(strIndex1);
+      signalData1.push_back(stod(strFirstValue1));
 
-      cout << "index int works: " << this->index << "\t first value in vector works: " << signalData[0] << endl;
+      cout << "index int works: " << this->index1 << "\t first value in vector works: " << signalData1[0] << endl;
 
-     } if (isThereIndex == 0)
+     } if (isThereIndex1 == 0)
      {
       cout << "no no index" << endl;
-      this->index = 0;
+      this->index1 = 0;
 
-      signalData.push_back(stod(firstLine));
+      signalData1.push_back(stod(firstLine1));
      }
     }
-    
-    //parse the rest into signalData
-      string line;
-      while (getline(fileToParse, line))
+
+
+
+
+      //parse and input validate first line
+      ifstream fileToParse2(inputfile2);
+
+      if(!fileToParse2)
       {
-          // Convert the line to a double and add it to the vector
-          double value = stod(line);
-          signalData.push_back(value);
+          cout << "could not open " << inputfile2 << " to be parsed" << endl;
+      } else
+      {
+          //first line parsing
+          string firstLine2;
+
+          getline(fileToParse2, firstLine2);
+
+          cout << "first line: \t" << firstLine2 << endl;
+
+          bool isThereIndex2 = firstLineInputValidation(firstLine2);
+
+          if (isThereIndex2 == 1)
+          {
+              cout << "yes theres index" << endl;
+
+              istringstream divideFirstLine(firstLine2);
+
+              string strIndex2, strFirstValue2;
+              divideFirstLine >> strIndex2 >> strFirstValue2;
+
+              cout << "index: " << strIndex2 << "\t first: " << strFirstValue2 << endl;
+
+              this->index2 = stoi(strIndex2);
+              signalData2.push_back(stod(strFirstValue2));
+
+              cout << "index int works: " << this->index2 << "\t first value in vector works: " << signalData2[0] << endl;
+
+          } if (isThereIndex2 == 0)
+          {
+              cout << "no no index" << endl;
+              this->index2 = 0;
+
+              signalData2.push_back(stod(firstLine2));
+          }
+      }
+
+
+
+    //parse the rest into signalData
+      string line1;
+      while (getline(fileToParse1, line1))
+      {
+          double value = stod(line1);
+          signalData1.push_back(value);
+      }
+
+      // Parse the rest into signalData for the second signal
+      string line2;
+      while (getline(fileToParse2, line2))
+      {
+          double value = stod(line2);
+          signalData2.push_back(value);
       }
     //set duration according to how big the vector is
-    duration = signalData.size();
-    cout << "Duration is "<< duration << endl;
+      duration1 = signalData1.size();
+      duration2 = signalData2.size();
+      cout << "Duration of the first signal is " << duration1 << endl;
+      cout << "Duration of the second signal is " << duration2 << endl;
   }
 };
 
@@ -128,15 +187,12 @@ int main(int argc, char* argv[]) {
  outputFileName = argv[3];
 
  Signal xData;
+ Signal yData;
 
- xData.parseFromFile(xDataFileName);
+ xData.parseFromFile(xDataFileName,yDataFileName);
+ 
 
-    // vector<Record> records = parseFromFiles(xDataFileName, yDataFileName);
-    // // Now, 'records' vector contains the parsed data
-    // for (const Record& rec : records) {
-    //     cout << "xraw: " << rec.xraw << ", yraw: " << rec.yraw << endl;
-    // }
-    // Implement cross-correlation here using 'records'
+
     return 0;
 }
 
