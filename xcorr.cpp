aa5429b99066f7b909a298fc7641e47bacd3b1f1
Katrice Mendoza <katrice.mendoza@obf.ateneo.edu>
2023-09-10 19:44:48 +0800

    tryna fix

--- xcorr.cpp
+++ xcorr.cpp
@@ -10,81 +10,117 @@
 
 using namespace std;
 
-struct Record {
-    int xraw;
-    int yraw;
+class Signal
+{
+ public:
+  vector<double> signalData;
+  int index;
+  int duration;
+
+  void parseFromFile(string inputfile)
+  {
+    //parse and input validate first line
+    ifstream fileToParse(inputfile);
+
+    if(!fileToParse)
+    {
+     cout << "could not open " << inputfile << " to be parsed" << endl;
+    } else
+    {
+     string firstLine;
+
+     cout << "first line: \t" << firstLine << endl;
+    }
+    //set index
+    //parse the rest into signalData
+    //set duration according to how big the vector is
+  }
 };
-vector<Record> parseFromFiles(const string& xDataFileName, const string& yDataFileName) {
-    vector<Record> records;
-    // Parse the x value file
-    ifstream xInputFile(xDataFileName);
-    if (!xInputFile) {
-        cerr << "Error opening the x value file." << endl;
-        return records;
-    }
-    string line;
-    while (getline(xInputFile, line)) {
-        try {
-            // Convert the line to an integer and store it in the vector
-            int xValue = stoi(line);
-            Record newRecord;
-            newRecord.xraw = xValue;
-            records.push_back(newRecord);
-        } catch (const invalid_argument& e) {
-            cerr << "Invalid data found in x value file: " << line << endl;
-        }
-    }
-    xInputFile.close();
-    // Parse the y value file
-    ifstream yInputFile(yDataFileName);
-    if (!yInputFile) {
-        cerr << "Error opening the y value file." << endl;
-        return records;
-    }
-    size_t currentIndex = 0; // Keep track of the current index in 'records'
-    while (getline(yInputFile, line)) {
-        try {
-            // Convert the line to an integer and store it in the vector
-            int yValue = stoi(line);
-            if (currentIndex < records.size()) {
-                // Assign the y value to the corresponding x value
-                records[currentIndex].yraw = yValue;
-                currentIndex++;
-            } else {
-                cerr << "More y values found than x values." << endl;
-                break; // Stop parsing if there are more y values than x values
-            }
-        } catch (const invalid_argument& e) {
-            cerr << "Invalid data found in y value file: " << line << endl;
-        }
-    }
-    yInputFile.close();
-    return records;
-}
+
+// struct Record {
+//     int xraw;
+//     int yraw;
+// };
+// vector<Record> parseFromFiles(const string& xDataFileName, const string& yDataFileName) {
+//     vector<Record> records;
+//     // Parse the x value file
+//     ifstream xInputFile(xDataFileName);
+//     if (!xInputFile) {
+//         cerr << "Error opening the x value file." << endl;
+//         return records;
+//     }
+//     string line;
+//     while (getline(xInputFile, line)) {
+//         try {
+//             // Convert the line to an integer and store it in the vector
+//             int xValue = stoi(line);
+//             Record newRecord;
+//             newRecord.xraw = xValue;
+//             records.push_back(newRecord);
+//         } catch (const invalid_argument& e) {
+//             cerr << "Invalid data found in x value file: " << line << endl;
+//         }
+//     }
+//     xInputFile.close();
+//     // Parse the y value file
+//     ifstream yInputFile(yDataFileName);
+//     if (!yInputFile) {
+//         cerr << "Error opening the y value file." << endl;
+//         return records;
+//     }
+//     size_t currentIndex = 0; // Keep track of the current index in 'records'
+//     while (getline(yInputFile, line)) {
+//         try {
+//             // Convert the line to an integer and store it in the vector
+//             int yValue = stoi(line);
+//             if (currentIndex < records.size()) {
+//                 // Assign the y value to the corresponding x value
+//                 records[currentIndex].yraw = yValue;
+//                 currentIndex++;
+//             } else {
+//                 cerr << "More y values found than x values." << endl;
+//                 break; // Stop parsing if there are more y values than x values
+//             }
+//         } catch (const invalid_argument& e) {
+//             cerr << "Invalid data found in y value file: " << line << endl;
+//         }
+//     }
+//     yInputFile.close();
+//     return records;
+// }
 void normCrossCorr()
 {
     //calculations go here
 }
+
 int main(int argc, char* argv[]) {
-    cout
-            << "please call the program in the format \n > xcorr [xdata] [ydata] [outputfile] \n where each of the bracketed terms are the respective file names."<< endl;
-    if (argc != 4) {
-        cerr << "Usage: " << argv[0] << " [xdata] [ydata] [output file]" << endl;
-        return 1;
-    }
-    string programName = argv[0];
-    string xDataFileName = argv[1];
-    string yDataFileName = argv[2];
-    string outputFileName = argv[3];
-    if (programName != "xcorr") {
-        cerr << "Usage: " << programName << " [xdata] [ydata] [output file]" << endl;
-        return 1;
-    }
-    vector<Record> records = parseFromFiles(xDataFileName, yDataFileName);
-    // Now, 'records' vector contains the parsed data
-    for (const Record& rec : records) {
-        cout << "xraw: " << rec.xraw << ", yraw: " << rec.yraw << endl;
-    }
+ if (argc == 4)
+ {
+  cout << "received file " << argv[1] << " for use as x values" << endl;
+  cout << "received file " << argv[2] << " for use as y values" << endl;
+  cout << "received file " << argv[3] << " for use as the output file" << endl;
+ } else
+ {
+  cout << "please call the program in the format \n > xcorr [xdata] [ydata] [outputfile] \n where each of the bracketed terms are the respective file names." << endl;
+
+  return 0;
+ }
+ 
+ string xDataFileName, yDataFileName, outputFileName;
+
+ xDataFileName = argv[1];
+ yDataFileName = argv[2];
+ outputFileName = argv[3];
+
+ Signal xData;
+
+ xData.parseFromFile(xDataFileName);
+
+    // vector<Record> records = parseFromFiles(xDataFileName, yDataFileName);
+    // // Now, 'records' vector contains the parsed data
+    // for (const Record& rec : records) {
+    //     cout << "xraw: " << rec.xraw << ", yraw: " << rec.yraw << endl;
+    // }
     // Implement cross-correlation here using 'records'
     return 0;
 }
