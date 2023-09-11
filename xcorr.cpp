bbf51db174915837af8d98817949aad2602692d4
Katrice Mendoza <katrice.mendoza@obf.ateneo.edu>
2023-09-11 09:18:04 +0800

    fixed

--- xcorr.cpp
+++ xcorr.cpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <vector>
 #include <fstream>
 #include <stdexcept>
@@ -15,18 +14,14 @@ using namespace std;
 bool firstLineInputValidation(string lineToCheck)
 {
  int spaceCount;
- bool firstC = false;
 
  for(char c : lineToCheck)
  {
-  if (firstC && isspace(c))
+  if (isspace(c))
   {
    spaceCount++;
   }
-  firstC = true;
  }
-
- cout << "space count: " << spaceCount << endl;
 
  if(spaceCount>0)
  {
@@ -40,122 +35,55 @@ bool firstLineInputValidation(string lineToCheck)
 class Signal
 {
  public:
-  vector<double> signalData1;
-  vector<double> signalData2;
-  int index1;
-  int index2;
-  size_t duration1;
-  size_t duration2;
+  vector<double> signalData;
+  
+  int index;
+  int duration;
 
   void parseFromFile(string inputfile1, string inputfile2)
   {
     //parse and input validate first line
-    ifstream fileToParse1(inputfile1);
+    ifstream fileToParse(inputfile1);
 
-    if(!fileToParse1)
+    if(!fileToParse)
     {
      cout << "could not open " << inputfile1 << " to be parsed" << endl;
     } else
     {
-     //first line parsing
-     string firstLine1;
+     string firstLine;
 
-     getline(fileToParse1, firstLine1);
+     getline(fileToParse, firstLine);
 
-     cout << "first line: \t" << firstLine1 << endl;
+     cout << "first line: \t" << firstLine << endl;
 
-     bool isThereIndex1 = firstLineInputValidation(firstLine1);
+     bool isThereIndex = firstLineInputValidation(firstLine);
 
-     if (isThereIndex1 == 1)
+     if (isThereIndex == 1)
      {
       cout << "yes theres index" << endl;
-
-      istringstream divideFirstLine(firstLine1);
-      
-      string strIndex1, strFirstValue1;
-      divideFirstLine >> strIndex1 >> strFirstValue1;
-
-      cout << "index: " << strIndex1 << "\t first: " << strFirstValue1 << endl;
-
-      this->index1 = stoi(strIndex1);
-      signalData1.push_back(stod(strFirstValue1));
-
-      cout << "index int works: " << this->index1 << "\t first value in vector works: " << signalData1[0] << endl;
-
-     } if (isThereIndex1 == 0)
+     } if (isThereIndex == 0)
      {
       cout << "no no index" << endl;
-      this->index1 = 0;
-
-      signalData1.push_back(stod(firstLine1));
      }
     }
-
-
-
-
-      //parse and input validate first line
-      ifstream fileToParse2(inputfile2);
-
-      if(!fileToParse2)
-      {
-          cout << "could not open " << inputfile2 << " to be parsed" << endl;
-      } else
-      {
-          //first line parsing
-          string firstLine2;
-
-          getline(fileToParse2, firstLine2);
-
-          cout << "first line: \t" << firstLine2 << endl;
-
-          bool isThereIndex2 = firstLineInputValidation(firstLine2);
-
-          if (isThereIndex2 == 1)
-          {
-              cout << "yes theres index" << endl;
-
-              istringstream divideFirstLine(firstLine2);
-
-              string strIndex2, strFirstValue2;
-              divideFirstLine >> strIndex2 >> strFirstValue2;
-
-              cout << "index: " << strIndex2 << "\t first: " << strFirstValue2 << endl;
-
-              this->index2 = stoi(strIndex2);
-              signalData2.push_back(stod(strFirstValue2));
-
-              cout << "index int works: " << this->index2 << "\t first value in vector works: " << signalData2[0] << endl;
-
-          } if (isThereIndex2 == 0)
-          {
-              cout << "no no index" << endl;
-              this->index2 = 0;
-
-              signalData2.push_back(stod(firstLine2));
-          }
-      }
-
-
-
+    //set index
     //parse the rest into signalData
-      string line1;
-      while (getline(fileToParse1, line1))
+      string line;
+      while (getline(fileToParse, line))
       {
-          double value = stod(line1);
-          signalData1.push_back(value);
+          double value = stod(line);
+          signalData.push_back(value);
       }
 
       // Parse the rest into signalData for the second signal
-      string line2;
-      while (getline(fileToParse2, line2))
+      string line;
+      while (getline(fileToParse, line))
       {
-          double value = stod(line2);
-          signalData2.push_back(value);
+          double value = stod(line);
+          signalData.push_back(value);
       }
     //set duration according to how big the vector is
-      duration1 = signalData1.size();
-      duration2 = signalData2.size();
+      duration = signalData1.size();
       cout << "Duration of the first signal is " << duration1 << endl;
       cout << "Duration of the second signal is " << duration2 << endl;
   }
